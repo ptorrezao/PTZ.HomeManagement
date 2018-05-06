5dcef87f1d2491b4c333d4dfeb589e12b8490a73
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-05-06 20:01:22 +0100

    Add Status Page

---
+++ PTZ.HomeManagement.Lib/Interfaces/ICoreService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTZ.HomeManagement.Interfaces
+{
+    public interface ICoreService
+    {
+        List<string> GetLastNMessages(int qtdOfMessages, string filename = null);
+    }
+}
---
+++ PTZ.HomeManagement/Utils/Conversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PTZ.HomeManagement.Utils
+{
+    public static class Conversion
+    {
+        internal static double ConvertBytesToMegabytes(long bytes, int? decimalPlaces)
+        {
+            return Math.Round(ConvertBytesToMegabytes(bytes), decimalPlaces ?? 10);
+        }
+
+        internal static double ConvertBytesToMegabytes(long bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+    }
+}
