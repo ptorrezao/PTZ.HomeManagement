2287fc646929ddcbc657190e1b06ac7327484abe
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-07-22 22:46:14 +0100

    SonarCloud

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -ev
+
+dotnet SonarScanner.MSBuild/SonarScanner.MSBuild.dll begin /k:"PTZ.HZ" /d:sonar.organization="ptorrezao-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=$1
 dotnet restore PTZ.HomeManagement.sln
 dotnet build PTZ.HomeManagement/PTZ.HomeManagement.csproj
+dotnet SonarScanner.MSBuild/SonarScanner.MSBuild.dll  end /d:sonar.login=$1
