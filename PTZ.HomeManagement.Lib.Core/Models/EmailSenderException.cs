df13262471a42fcf750821517d6eb174c23301b2
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-08-12 17:42:34 +0100

    Fix Build

--- PTZ.HomeManagement.Lib.Core/Models/EmailSenderException.cs
+++ PTZ.HomeManagement.Lib.Core/Models/EmailSenderException.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace PTZ.HomeManagement.Models
 {
     public class EmailSenderException : Exception
     {
+        public EmailSenderException(string message) : base(message)
+        {
+        }
     }
 }
