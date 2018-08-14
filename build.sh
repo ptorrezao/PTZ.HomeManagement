837d3091beee33a0db8d22241c211b955a2ce987
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-08-14 23:37:36 +0100

    Try Add test to build

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ev
 
-chmod +x SonarScanner.MSBuild/sonar-scanner-3.2.0.1227/bin/sonar-scanner
-dotnet SonarScanner.MSBuild/SonarScanner.MSBuild.dll begin /k:"PTZ.HZ" /d:sonar.organization="ptorrezao-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cpd.exclusions="**/PTZ.HomeManagement.Lib/Data/Migrations/**" /d:sonar.exclusions="**/PTZ.HomeManagement/lib/**,PTZ.HomeManagement/lib/**,**/*bootstrap*/**/*,**/*jquery*,**/*datatables/**/**/*" /d:sonar.login=$1
-dotnet restore PTZ.HomeManagement.sln
+dotnet SonarScanner.MSBuild/SonarScanner.MSBuild.dll begin /k:"PTZ.HZ" /d:sonar.organization="ptorrezao-github" /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cpd.exclusions="**/PTZ.HomeManagement.Lib/Data/Migrations/**" /d:sonar.exclusions="**/PTZ.HomeManagement/lib/**,PTZ.HomeManagement/lib/**,**/*bootstrap*/**/*,**/*jquery*,**/*datatables/**/**/*" /d:sonar.login=$1
 dotnet build PTZ.HomeManagement.sln
+dotnet test PTZ.HomeManagement.MyFinance.Services.Test/PTZ.HomeManagement.MyFinance.Services.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=lcov
 dotnet SonarScanner.MSBuild/SonarScanner.MSBuild.dll  end /d:sonar.login=$1
