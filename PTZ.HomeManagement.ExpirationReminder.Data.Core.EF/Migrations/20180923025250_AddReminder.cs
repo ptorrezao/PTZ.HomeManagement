c1e732b677c45cf62e14e5ee309b930a3cbb815f
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-09-23 04:36:19 +0100

    Add Reminder Module

---
+++ PTZ.HomeManagement.ExpirationReminder.Data.Core.EF/Migrations/20180923025250_AddReminder.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PTZ.HomeManagement.ExpirationReminder.Data.Core.EF.Migrations
+{
+    public partial class AddReminder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "Reminder");
+
+            //migrationBuilder.CreateTable(
+            //    name: "AspNetUsers",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<string>(nullable: false),
+            //        AccessFailedCount = table.Column<int>(nullable: false),
+            //        ConcurrencyStamp = table.Column<string>(nullable: true),
+            //        Email = table.Column<string>(nullable: true),
+            //        EmailConfirmed = table.Column<bool>(nullable: false),
+            //        FirstName = table.Column<string>(nullable: true),
+            //        LastName = table.Column<string>(nullable: true),
+            //        LockoutEnabled = table.Column<bool>(nullable: false),
+            //        LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+            //        NormalizedEmail = table.Column<string>(nullable: true),
+            //        NormalizedUserName = table.Column<string>(nullable: true),
+            //        PasswordHash = table.Column<string>(nullable: true),
+            //        PhoneNumber = table.Column<string>(nullable: true),
+            //        PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+            //        RequirePasswordChange = table.Column<bool>(nullable: false),
+            //        SecurityStamp = table.Column<string>(nullable: true),
+            //        TwoFactorEnabled = table.Column<bool>(nullable: false),
+            //        UserName = table.Column<string>(nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+            //    });
+
+            migrationBuilder.CreateTable(
+                name: "Reminders",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false, defaultValueSql: "nextval('\"Reminder\"')"),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    ExpirationDate = table.Column<DateTime>(nullable: false),
+                    ReminderType = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reminders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reminders_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reminders_ApplicationUserId",
+                table: "Reminders",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reminders");
+
+            //migrationBuilder.DropTable(
+            //    name: "AspNetUsers");
+
+            migrationBuilder.DropSequence(
+                name: "Reminder");
+        }
+    }
+}
---
+++ PTZ.HomeManagement/Models/ExpirationReminderModels/ReminderListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PTZ.HomeManagement.Models.ExpirationReminderModels
+{
+    public class ReminderListViewModel
+    {
+        public List<ReminderListItemViewModel> Items { get; set; }
+    }
+}
