df056a4bbb572ecaa446bd51d39ac6f8984a5fb7
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-10-28 04:25:44 +0000

    SonarCloud Suggestions

--- PTZ.HomeManagement/Components/MyFinance/CategoriesDistribution.cs
+++ PTZ.HomeManagement/Components/MyFinance/CategoriesDistribution.cs
@@ -34,7 +34,7 @@ namespace PTZ.HomeManagement.Components.MyFinance
                 bankAccounts.Where(x => x.IsVisible).ToList().ForEach(bankAccount =>
                 {
                     var categories = _myFinanceService.GetCategories(User.GetUserId());
-                    DateTime lastPaymentDay = now.Day > 24 ? new DateTime(now.Year, now.Month, 24) : new DateTime(now.Year, now.Month, 24).AddMonths(-1);
+                    DateTime lastPaymentDay = FindLastPaymentDay(now);
                     bankAccount.Movements = _myFinanceService.GetBankAccountMovements(User.GetUserId(), bankAccount.Id, lastPaymentDay, now);
 
                     foreach (var selectedCategory in categories)
@@ -45,23 +45,7 @@ namespace PTZ.HomeManagement.Components.MyFinance
                             {
                                 var items = x.Where(e => e.Categories != null && e.Categories.Any(r => r.CategoryId == selectedCategory.Id));
 
-                                if (items.Any())
-                                {
-                                    var amout = -items.Sum(q => q.Amount);
-
-                                    if (amout > 0)
-                                    {
-                                        FindDates(vm, items.Min(q => q.MovementDate), items.Max(q => q.MovementDate));
-
-                                        vm.Items.Add(new BarChartItemViewModel()
-                                        {
-                                            Label = selectedCategory.Name,
-                                            Color = bankAccount.Color,
-                                            Value = amout,
-                                            Group = bankAccount.Bank.ToString()
-                                        });
-                                    }
-                                }
+                                AddItemIfAnyItem(bankAccount, vm, selectedCategory, items);
                             }
                         });
                     }
@@ -71,6 +55,32 @@ namespace PTZ.HomeManagement.Components.MyFinance
             });
 
             return View(viewModel);
+        }
+
+        private DateTime FindLastPaymentDay(DateTime now)
+        {
+            return now.Day > 24 ? new DateTime(now.Year, now.Month, 24) : new DateTime(now.Year, now.Month, 24).AddMonths(-1);
+        }
+
+        private void AddItemIfAnyItem(BankAccount bankAccount, BarChartViewModel vm, Category selectedCategory, IEnumerable<BankAccountMovement> items)
+        {
+            if (items.Any())
+            {
+                var amout = -items.Sum(q => q.Amount);
+
+                if (amout > 0)
+                {
+                    FindDates(vm, items.Min(q => q.MovementDate), items.Max(q => q.MovementDate));
+
+                    vm.Items.Add(new BarChartItemViewModel()
+                    {
+                        Label = selectedCategory.Name,
+                        Color = bankAccount.Color,
+                        Value = amout,
+                        Group = bankAccount.Bank.ToString()
+                    });
+                }
+            }
         }
 
         private void FindDates(BarChartViewModel vm, DateTime minDate, DateTime maxDate)
